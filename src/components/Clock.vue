91d70f186ba4a4eeb5f3a69aa19538250f551eeb
jsantt <35174533+jsantt@users.noreply.github.com>
2019-10-14 22:33:51 +0300

    convert to use vue.js

---
+++ src/components/Clock.vue
@@ -0,0 +1,37 @@
+<style scoped>
+div {
+  top: 0;
+  position: fixed;
+  left: 0;
+  background-color: #e2e1e1;
+  padding: 0.5rem 1rem;
+}
+</style>
+<template>
+  <div>{{time}}</div>
+</template>
+
+<script>
+export default {
+  name: "Clock",
+  props: {
+    stop: Object
+  },
+  data() {
+    return {
+      time: ""
+    };
+  },
+  mounted: function() {
+    this.startClock();
+  },
+  methods: {
+    startClock: function() {
+      setInterval(() => {
+        const now = new Date();
+        this.time = now.toLocaleTimeString("fi-FI");
+      }, 1000);
+    }
+  }
+};
+</script>

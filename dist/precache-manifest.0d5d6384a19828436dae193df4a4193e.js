2ab911867d60f396dcc6a1bf3c7d87f9040792c9
jsantt <35174533+jsantt@users.noreply.github.com>
2019-10-14 22:36:10 +0300

    add dist directory

---
+++ dist/precache-manifest.0d5d6384a19828436dae193df4a4193e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "924ab64fa61fa34d01de",
+    "url": "/js/chunk-vendors.ec12355f.js"
+  },
+  {
+    "revision": "3d481654834d19029316",
+    "url": "/js/app.1c142015.js"
+  },
+  {
+    "revision": "ac690aa9692576cc4406acbe251fced5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d481654834d19029316",
+    "url": "/css/app.ba19b4de.css"
+  }
+];

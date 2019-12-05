8e768f21281d9bf8b8a9b572d871594530bb34b4
jsantt <35174533+jsantt@users.noreply.github.com>
2019-12-05 22:30:27 +0200

    fix vue config

---
+++ docs/precache-manifest.cbea8408c3beaa285af0d6e369df1117.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "be24baac2de205f4a959",
+    "url": "/js/chunk-vendors.060d14a5.js"
+  },
+  {
+    "revision": "1a232e085b673b3cd058",
+    "url": "/js/app.6bf659cd.js"
+  },
+  {
+    "revision": "edfa094faa280471af23d7bffd940d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b851cc7e8aae9ccff36e4bad22b9e795",
+    "url": "/img/logo-master.svg"
+  },
+  {
+    "revision": "8a6a8e4410b21162031e0d688b802f42",
+    "url": "/img/bus-512x512.png"
+  },
+  {
+    "revision": "7245366af97e06704954c35909ca079f",
+    "url": "/img/bus-192x192.png"
+  },
+  {
+    "revision": "6efcc11e754f068bf369decccabefc14",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "1a232e085b673b3cd058",
+    "url": "/css/app.4d66788f.css"
+  },
+  {
+    "revision": "bb44d38bd690831ccae6b3d8e8edf7ae",
+    "url": "/CNAME"
+  }
+];
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ module.exports = {
   /*publicPath: process.env.NODE_ENV === "production" ? "/stops" : "",*/
   outputDir: "docs",
   pwa: {
-    workboxOptions: {
-      workboxOptions: { skipWaiting: true } /* fix ios cache issue*/
-    }
+    workboxOptions: { skipWaiting: true } /* fix ios cache issue*/
   }
 };

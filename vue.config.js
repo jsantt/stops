639db7c156b38c7c669260d75cafa1afa26b515c
jsantt <35174533+jsantt@users.noreply.github.com>
2019-12-05 20:47:23 +0200

    solve ios cache issue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   /*publicPath: process.env.NODE_ENV === "production" ? "/stops" : "",*/
-  outputDir: "docs"
+  outputDir: "docs",
+  pwa: {
+    workboxOptions: {
+      workboxOptions: { skipWaiting: true } /* fix ios cache issue*/
+    }
+  }
 };
